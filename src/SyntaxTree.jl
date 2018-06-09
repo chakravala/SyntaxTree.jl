eb50a8022bdc22af7e20182d02579e67d94e4bdb
Michael Reed <michaelreed@crucialflow.com>
2018-06-09 11:46:17 -0400

    transferred linefilter method

--- src/SyntaxTree.jl
+++ src/SyntaxTree.jl
@@ -1,5 +1,32 @@
 module SyntaxTree
 
-# package code goes here
+export linefilter
+
+"""
+    linefilter(::Expr)
+
+Recursively filters out :line blocks from Expr objects
+"""
+@noinline function linefilter(e::Expr)
+    total = length(e.args)
+    i = 0
+    while i < total
+        i += 1
+        if e.args[i] |> typeof == Expr
+            if e.args[i].head == :line
+                deleteat!(e.args,i)
+                total -= 1
+                i -= 1
+            else
+                e.args[i] = linefilter(e.args[i])
+            end
+        elseif e.args[i] |> typeof == LineNumberNode
+            deleteat!(e.args,i)
+            total -= 1
+            i -= 1
+        end
+    end
+    return e
+end
 
 end # module
